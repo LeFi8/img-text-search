37475ca35fc1b2c6f54e2a70022f8625706b1d41
lef8 <alan.sipiorski14@gmail.com>
2023-10-08 16:20:28 +0200

    Fix error on aborting screenshot

--- src/main.py
+++ src/main.py
@@ -6,14 +6,16 @@ from screen_capture import ScreenCapture
 
 
 def main():
-    tmp_path = "tmp"
-
-    if not os.path.exists(tmp_path):
-        os.mkdir(tmp_path)
-
     root = tk.Tk()
     screen_capture = ScreenCapture(root=root)
     root.mainloop()
+
+    if not screen_capture.get_capture_area_set():
+        return
+
+    tmp_path = "tmp"
+    if not os.path.exists(tmp_path):
+        os.mkdir(tmp_path)
 
     screenshot = pyautogui.screenshot(
         region=screen_capture.get_captured_area())
