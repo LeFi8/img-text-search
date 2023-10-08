1edf0ad12ccd22b65f2448df8331c2951666fa9e
lef8 <alan.sipiorski14@gmail.com>
2023-10-08 14:50:19 +0200

    Hide private attributes

--- src/screen_capture.py
+++ src/screen_capture.py
@@ -3,52 +3,53 @@ import tkinter as tk
 
 class ScreenCapture:
     def __init__(self, root):
-        self.root = root
-        self.root.attributes('-fullscreen', True)
-        self.root.attributes('-alpha', 0.1)
+        self._root = root
+        self._root.attributes('-fullscreen', True)
+        self._root.attributes('-alpha', 0.1)
 
-        self.canvas = tk.Canvas(root, cursor="cross")
-        self.canvas.pack(fill="both", expand=True)
-        self.canvas.bind("<ButtonPress-1>", self._on_press)
-        self.canvas.bind("<B1-Motion>", self._on_drag)
-        self.canvas.bind("<ButtonRelease-1>", self._on_release)
+        self._canvas = tk.Canvas(root, cursor="cross")
+        self._canvas.pack(fill="both", expand=True)
+        self._canvas.bind("<ButtonPress-1>", self._on_press)
+        self._canvas.bind("<B1-Motion>", self._on_drag)
+        self._canvas.bind("<ButtonRelease-1>", self._on_release)
 
-        self.root.bind("<Escape>", self._exit)
-        self.root.bind("<ButtonPress-3>", self._exit)
+        self._root.bind("<Escape>", self._exit)
+        self._root.bind("<ButtonPress-3>", self._exit)
 
-        self.start_x, self.start_y = None, None
-        self.end_x, self.end_y = None, None
-        self.rect = None
+        self._start_x, self._start_y = None, None
+        self._end_x, self._end_y = None, None
+        self._rect = None
 
     def _on_press(self, event):
-        self.start_x = self.canvas.canvasx(event.x)
-        self.start_y = self.canvas.canvasy(event.y)
+        self._start_x = self._canvas.canvasx(event.x)
+        self._start_y = self._canvas.canvasy(event.y)
 
-        if self.rect:
-            self.canvas.delete(self.rect)
+        if self._rect:
+            self._canvas.delete(self._rect)
 
-        self.rect = self.canvas.create_rectangle(
-            self.start_x, self.start_y, self.start_x, self.start_y, outline="red", width=3)
+        self._rect = self._canvas.create_rectangle(
+            self._start_x, self._start_y, self._start_x, self._start_y, outline="red", width=3)
 
     def _on_drag(self, event):
-        self.end_x = self.canvas.canvasx(event.x)
-        self.end_y = self.canvas.canvasy(event.y)
+        self._end_x = self._canvas.canvasx(event.x)
+        self._end_y = self._canvas.canvasy(event.y)
 
-        self.canvas.coords(self.rect, self.start_x,
-                           self.start_y, self.end_x, self.end_y)
+        self._canvas.coords(self._rect, self._start_x,
+                            self._start_y, self._end_x, self._end_y)
 
     def _on_release(self, event):
-        self.end_x = self.canvas.canvasx(event.x)
-        self.end_y = self.canvas.canvasy(event.y)
+        self._end_x = self._canvas.canvasx(event.x)
+        self._end_y = self._canvas.canvasy(event.y)
 
-        selected_area = (self.start_x, self.start_y, self.end_x, self.end_y)
+        selected_area = (self._start_x, self._start_y,
+                         self._end_x, self._end_y)
 
         self._capture_area(selected_area)
-        self.canvas.delete(self.rect)
+        self._canvas.delete(self._rect)
         self._exit()
 
-    def _exit(self, event=None):
-        self.root.destroy()
+    def _exit(self):
+        self._root.destroy()
 
     def _capture_area(self, area):
         x1, y1, x2, y2 = area
